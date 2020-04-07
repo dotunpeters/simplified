201ed9935b435b873ad3d968298f1f667018f099
dotun peter <oyelakindotun@gmail.com>
2020-04-07 11:40:09 +0100

    refactoring

--- test_application.py
+++ test_application.py
@@ -1,8 +1,28 @@
 import unittest
+import application
+from application import app
+import requests
+import os
 
-class Test_test_1(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+class Test_more_route(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    def test_more(self):
+        data = {"page": 2}
+        requests.get("http://127.0.0.1:5000/")
+        result = requests.post("http://127.0.0.1:5000/more", data=data).json()
+        self.assertEqual(result[0]["success"], True)
 
 if __name__ == '__main__':
     unittest.main()
